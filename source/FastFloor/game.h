acf0a92a1010213bc764fbd82fd26236bd56f47e
JayDalton <github@t-jay.net>
2021-08-25 21:33:28 +0200

    + game + engine + SDL2

---
+++ source/FastFloor/game.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "engine.h"
+
+class FastFloor : public GameEngine
+{
+public:
+
+   bool createUser() override
+   {
+      return true;
+   }
+
+   bool updateUser() override
+   {
+
+      return true;
+   }
+
+private:
+};
