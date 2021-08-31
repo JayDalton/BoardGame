5c92e8065c41f621e8743ad1abb0282cf9295b3a
JayDalton <github@t-jay.net>
2021-08-31 22:41:57 +0200

    * more Shader helper inputs

--- source/FastFloor/source/shapes/base/shader.cpp
+++ source/FastFloor/source/shapes/base/shader.cpp
@@ -63,6 +63,51 @@ void ogl::Shader::setFloat(const std::string& name, float value) const
    glUniform1f(glGetUniformLocation(m_id, name.c_str()), value);
 }
 
+void ogl::Shader::setVec2(const std::string& name, const glm::vec2& value) const
+{
+   glUniform2fv(glGetUniformLocation(m_id, name.c_str()), 1, &value[0]);
+}
+
+void ogl::Shader::setVec2(const std::string& name, float x, float y) const
+{
+   glUniform2f(glGetUniformLocation(m_id, name.c_str()), x, y);
+}
+
+void ogl::Shader::setVec3(const std::string& name, const glm::vec3& value) const
+{
+   glUniform3fv(glGetUniformLocation(m_id, name.c_str()), 1, &value[0]);
+}
+
+void ogl::Shader::setVec3(const std::string& name, float x, float y, float z) const
+{
+   glUniform3f(glGetUniformLocation(m_id, name.c_str()), x, y, z);
+}
+
+void ogl::Shader::setVec4(const std::string& name, float x, float y, float z, float w)
+{
+   glUniform4f(glGetUniformLocation(m_id, name.c_str()), x, y, z, w);
+}
+
+void ogl::Shader::setVec4(const std::string& name, const glm::vec4& value) const
+{
+   glUniform4fv(glGetUniformLocation(m_id, name.c_str()), 1, &value[0]);
+}
+
+void ogl::Shader::setMat2(const std::string& name, const glm::mat2& mat) const
+{
+   glUniformMatrix2fv(glGetUniformLocation(m_id, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+}
+
+void ogl::Shader::setMat3(const std::string& name, const glm::mat3& mat) const
+{
+   glUniformMatrix3fv(glGetUniformLocation(m_id, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+}
+
+void ogl::Shader::setMat4(const std::string& name, const glm::mat4& mat) const
+{
+   glUniformMatrix4fv(glGetUniformLocation(m_id, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+}
+
 void ogl::Shader::checkCompileErrors(unsigned int shader, Type type)
 {
    int success{ false };
