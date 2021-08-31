5b9555b6a1fbc33312f88d9071ebcd7ad5a50353
JayDalton <github@t-jay.net>
2021-08-31 22:41:33 +0200

    * add glm ext.hpp to common

--- source/FastFloor/source/shapes/base/common.hpp
+++ source/FastFloor/source/shapes/base/common.hpp
@@ -15,9 +15,10 @@
 #include <filesystem>
 
 #include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtx/transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
+#include <glm/ext.hpp>
+//#include <glm/gtc/matrix_transform.hpp>
+//#include <glm/gtx/transform.hpp>
+//#include <glm/gtc/type_ptr.hpp>
 
 #include <GL/glew.h>
 
