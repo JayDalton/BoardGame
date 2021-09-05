68a59ec3b688bbe0504df0a444e5c6dfe59d2ecb
JayDalton <github@t-jay.net>
2021-09-06 00:03:02 +0200

    * fix PNG type + vert flip

--- source/FastFloor/source/shapes/base/texture.cpp
+++ source/FastFloor/source/shapes/base/texture.cpp
@@ -51,11 +51,12 @@ bool ogl::Texture::createTexture(std::string_view path)
    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
    // load image, create texture and generate mipmaps
+   stbi_set_flip_vertically_on_load(true);//cause images the way OpenGL expects
    if (auto* data = stbi_load(path.data(), &m_width, &m_height, &m_compo, 0))
    {
       m_type = (m_compo == 4) ? GL_RGBA : (m_compo == 3) ? GL_RGB : GL_NONE;
 
-      glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, m_width, m_height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+      glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, m_width, m_height, 0, m_type, GL_UNSIGNED_BYTE, data);
       glGenerateMipmap(GL_TEXTURE_2D);
       stbi_image_free(data);
       return true;
