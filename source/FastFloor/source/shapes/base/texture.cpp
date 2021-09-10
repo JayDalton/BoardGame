8be406c1e4f5ac828e2d5e21aca20ba87bc6237e
Jay Desktop <github@t-jay.net>
2021-09-10 23:41:12 +0200

    * remove comments

--- source/FastFloor/source/shapes/base/texture.cpp
+++ source/FastFloor/source/shapes/base/texture.cpp
@@ -5,32 +5,6 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
 
-//ogl::Texture::Texture(Size size, Color color)
-//   : m_width(size.x), m_height(size.y)
-//   , m_compo(4), m_type(GL_RGBA)
-//{
-//   glGenTextures(1, &m_id);
-//   glBindTexture(GL_TEXTURE_2D, m_id);
-//
-//   // set the texture wrapping parameters
-//   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE); // note that we set the container wrapping method to GL_CLAMP_TO_EDGE
-//   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-//
-//   // set texture filtering parameters
-//   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-//   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-//
-//   int length{ m_compo * m_width * m_height };
-//   std::vector<std::uint8_t> buffer(length, 1);
-//
-//   glTexImage2D(GL_TEXTURE_2D, 0, m_compo, m_width, m_height, 0, m_type, GL_UNSIGNED_BYTE, buffer.data());
-//   //glGenerateMipmap(GL_TEXTURE_2D);
-//}
-
-//ogl::Texture::Texture(std::filesystem::path path)
-//{
-//}
-
 ogl::Texture::~Texture()
 {
    glDeleteTextures(1, &m_id);
--- source/FastFloor/source/shapes/base/texture.hpp
+++ source/FastFloor/source/shapes/base/texture.hpp
@@ -9,15 +9,9 @@ namespace ogl
       unsigned int m_id{ 0 };
 
       ~Texture();
-      //Texture() = default;
-
-      //explicit Texture(std::filesystem::path path);
-      //explicit Texture(Size size, Color color);
 
    protected:
       bool createTexture(std::string_view path);
-
-      friend std::ostream& operator<<(std::ostream& os, const Texture& tex);
 
   	   std::string getTypeName() const;
 
@@ -27,16 +21,6 @@ namespace ogl
   	   int m_height{ 0 };
   	   unsigned m_type{ 0 }; // rgb(a)
   };
-
-   //std::ostream& operator<<(std::ostream& os, const Texture& tex)
-   //{
-   //   // write obj to stream
-   //   os << "ID: " << tex.m_id;
-   //   os << " W: " << tex.m_width;
-   //   os << " H: " << tex.m_height;
-
-   //   return os;
-   //}
 }
 
 
